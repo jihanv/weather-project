936006771192a6c60d0bf2b802e17d14203e3a15
jihanv <hedjmny@gmail.com>
2025-11-25 19:16:50 +0900

    50:34 Remove unnecessary div in DailyForecast.

--- src/components/cards/DailyForecast.tsx
+++ src/components/cards/DailyForecast.tsx
@@ -19,29 +19,29 @@ export default function DailyForecast() {
     return (
 
         <>
-            <Card title="Daily Forecast">
-                <div className='flex flex-col gap-4'>
-                    {data.daily.map((day) => {
-                        return (
-                            <div
-                                key={day.dt}
-                                className='flex justify-between'
-                            >
-                                <p className='w-9'>{new Date(day.dt * 1000).toLocaleDateString(undefined, {
-                                    weekday: "short",
+            <Card title="Daily Forecast" childrenClassName='flex flex-col gap-4'>
 
-                                })}</p>
-                                <img
-                                    className='size-8'
-                                    src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                                    alt="Weather Icon" />
-                                <p>{Math.round(day.temp.day)}°C</p>
-                                <p className='text-gray-500/75'>{Math.round(day.temp.min)}°C</p>
-                                <p className='text-gray-500/75'>{Math.round(day.temp.max)}°C</p>
-                            </div>
-                        )
-                    })}
-                </div>
+                {data.daily.map((day) => {
+                    return (
+                        <div
+                            key={day.dt}
+                            className='flex justify-between'
+                        >
+                            <p className='w-9'>{new Date(day.dt * 1000).toLocaleDateString(undefined, {
+                                weekday: "short",
+
+                            })}</p>
+                            <img
+                                className='size-8'
+                                src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                                alt="Weather Icon" />
+                            <p>{Math.round(day.temp.day)}°C</p>
+                            <p className='text-gray-500/75'>{Math.round(day.temp.min)}°C</p>
+                            <p className='text-gray-500/75'>{Math.round(day.temp.max)}°C</p>
+                        </div>
+                    )
+                })}
+
             </Card>
         </>
     )
