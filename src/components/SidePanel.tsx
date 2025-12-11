d280c56730da1bceebd5e05fca2a0687cf74e240
jihanv <hedjmny@gmail.com>
2025-12-11 21:13:38 +0900

    4:04 Add sliders to give context to airpollution information.

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -3,6 +3,7 @@ import type { Coordinates } from '@/types'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { Suspense } from 'react'
 import Card from './cards/Card'
+import { Slider } from './ui/slider'
 
 type Props = {
     coords: Coordinates
@@ -24,7 +25,7 @@ export default SidePanel
 function AirPollution({ coords }: Props) {
 
     const { data } = useSuspenseQuery({
-        queryKey: ["pollution"],
+        queryKey: ["pollution", coords],
         queryFn: () => getAirPollution(coords)
     })
     return (
@@ -34,14 +35,26 @@ function AirPollution({ coords }: Props) {
                 <h1 className='text-5xl font-semibold'>{data.list[0].main.aqi}</h1>
                 <h1 className='text-2xl font-semibold'>AQI</h1>
                 {Object.entries(data.list[0].components).map(([key, value]) => {
+                    const pollutant = airQualityRanges[key.toUpperCase() as keyof typeof airQualityRanges];
+                    const max = Math.max(pollutant["Very Poor"].min, value)
                     return (
                         <>
-                            <Card key={key} className='hover:scale-105 transition-transform duration-300 from-sidebar-accent to-sidebar-accent/60'>
-                                <div>
+                            <Card
+                                key={key}
+                                className='hover:scale-105 transition-transform duration-300 from-sidebar-accent to-sidebar-accent/60 gap-0!'
+                                childrenClassName='flex flex-col gap-3'
+                            >
+                                <div className='flex justify-between'>
                                     <span className='text-lg font-bold capitalize'>{key}</span>
                                     <span className='text-lg font-semibold'>{value}</span>
                                 </div>
+                                <Slider min={0} max={max} value={[value]} disabled />
+                                <div className='flex justify-between text-xs'>
+                                    <p>0</p>
+                                    <p>{max}</p>
+                                </div>
                             </Card>
+
                         </>
                     )
                 })}
@@ -49,4 +62,85 @@ function AirPollution({ coords }: Props) {
             </div>
         </>
     )
+}
+
+type AirQualityLevel = "Good" | "Fair" | "Moderate" | "Poor" | "Very Poor"
+
+interface Range {
+    min: number
+    max: number | null
+}
+
+type Pollutant = "SO2" | "NO2" | "PM10" | "PM2_5" | "O3" | "CO" | "NO" | "NH3"
+
+type AirQualityRanges = Record<Pollutant, Record<AirQualityLevel, Range>>
+
+const airQualityRanges: AirQualityRanges = {
+    SO2: {
+        Good: { min: 0, max: 20 },
+        Fair: { min: 20, max: 80 },
+        Moderate: { min: 80, max: 250 },
+        Poor: { min: 250, max: 350 },
+        "Very Poor": { min: 350, max: null },
+    },
+    NO2: {
+        Good: { min: 0, max: 40 },
+        Fair: { min: 40, max: 70 },
+        Moderate: { min: 70, max: 150 },
+        Poor: { min: 150, max: 200 },
+        "Very Poor": { min: 200, max: null },
+    },
+    PM10: {
+        Good: { min: 0, max: 20 },
+        Fair: { min: 20, max: 50 },
+        Moderate: { min: 50, max: 100 },
+        Poor: { min: 100, max: 200 },
+        "Very Poor": { min: 200, max: null },
+    },
+    PM2_5: {
+        Good: { min: 0, max: 10 },
+        Fair: { min: 10, max: 25 },
+        Moderate: { min: 25, max: 50 },
+        Poor: { min: 50, max: 75 },
+        "Very Poor": { min: 75, max: null },
+    },
+    O3: {
+        Good: { min: 0, max: 60 },
+        Fair: { min: 60, max: 100 },
+        Moderate: { min: 100, max: 140 },
+        Poor: { min: 140, max: 180 },
+        "Very Poor": { min: 180, max: null },
+    },
+    CO: {
+        Good: { min: 0, max: 4400 },
+        Fair: { min: 4400, max: 9400 },
+        Moderate: { min: 9400, max: 12400 },
+        Poor: { min: 12400, max: 15400 },
+        "Very Poor": { min: 15400, max: null },
+    },
+    NO: {
+        Good: { min: 0, max: 20 },
+        Fair: { min: 20, max: 40 },
+        Moderate: { min: 40, max: 60 },
+        Poor: { min: 60, max: 80 },
+        "Very Poor": { min: 80, max: null },
+    },
+    NH3: {
+        Good: { min: 0, max: 40 },
+        Fair: { min: 40, max: 70 },
+        Moderate: { min: 70, max: 150 },
+        Poor: { min: 150, max: 200 },
+        "Very Poor": { min: 200, max: null },
+    },
+}
+
+const pollutantNameMapping: Record<Pollutant, string> = {
+    SO2: "Sulfur dioxide",
+    NO2: "Nitrogen dioxide",
+    PM10: "Particulate matter 10",
+    PM2_5: "Fine particles matter",
+    O3: "Ozone",
+    CO: "Carbon monoxide",
+    NO: "Nitrogen monoxide",
+    NH3: "Ammonia",
 }
