36e3afa27e68eb285db11a35cd7e223e8891eb5f
jihanv <hedjmny@gmail.com>
2025-12-18 22:00:13 +0900

    5:14 Increase size of text and Icon in hourly forecast for larger screens

--- src/components/cards/HourlyForecast.tsx
+++ src/components/cards/HourlyForecast.tsx
@@ -18,14 +18,14 @@ export default function HourlyForecast({ coords }: ForecastProps) {
         <>
             <Card title="Hourly Forecast (48 Hours)" childrenClassName='flex flex-row gap-6 overflow-x-scroll'>
                 {data.hourly.map((hour) => (
-                    <div className='flex flex-col gap-2 items-center p-2' key={hour.dt.toString()}>
-                        <p className='whitespace-nowrap' >{new Date(hour.dt * 1000).toLocaleTimeString(undefined, {
+                    <div className='flex flex-col gap-2 items-center p-2 2xl:justify-between' key={hour.dt.toString()}>
+                        <p className='whitespace-nowrap 2xl:scale-110' >{new Date(hour.dt * 1000).toLocaleTimeString(undefined, {
                             hour: "numeric",
                             minute: "2-digit",
                             hour12: true
                         })}</p>
-                        <WeatherIcon alt="Weather Icon" src={hour.weather[0].icon} />
-                        <p>{Math.round(hour.temp)}°C</p>
+                        <WeatherIcon className='2xl:size-10' alt="Weather Icon" src={hour.weather[0].icon} />
+                        <p className='2xl:scale-110'>{Math.round(hour.temp)}°C</p>
                     </div>
                 ))}
             </Card>
